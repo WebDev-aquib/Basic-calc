d8b5acfdad174d71c4fb2e99743d61094c1878d6
Md Aquib Ansari <heartlesseshal891@gmail.com>
2021-03-20 13:17:49 +0530

    Revert "Update Basic Calculator.py"

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -3,13 +3,13 @@ op=input("Enter operator:")
 num2=float(input("Enter second number:"))
 
 if op== "+":
-    print("Sum is", num1 + num2)
+    print(num1 + num2)
 elif op== "-":
-    print("Subtraction is", num1 - num2)
+    print(num1 - num2)
 elif op=="*":
-    print("Multiplication is", num1 * num2)
+    print(num1 * num2)
 elif op== "/":
-    print("Division is ", num1 / num2)
+    print(num1 / num2)
 elif op=="%":
     print(num1 % num2)
 elif op=="**":
